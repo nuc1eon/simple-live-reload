f905a0d72503e6ce53c9cc55337daa59dd334e93
example@example.com <example@example.com>
2025-04-24 14:35:18 +1000

    reword test

--- test/run.js
+++ test/run.js
@@ -9,7 +9,7 @@ const server = app.listen(8080, async () => {
   spawn("chromium", ["http://localhost:8080/start"], { shell: true });
   await delay(1000);
 
-  await test("reloads page when page is updated", async () => {
+  await test("reloads page when HTML is updated", async () => {
     const t = startTest({
       files: {
         "/index.html": {
